6ef3142711c6f1b73bceb32f4905199b978dd73e
SamuelDarras <slamicote@gmail.com>
2022-04-21 09:15:17 +0200

    moved connection attempt in a separate function

--- index.cjs
+++ index.cjs
@@ -1,44 +1,50 @@
 const BluetoothClassicSerialportClient = require("bluetooth-classic-serialport-client")
 const { UM34C } = require("./um34c.cjs")
 
+function attemptConnect(serial, device) {
+    if (device .name === "UM34C") {
+
+        serial.connect(device.address)
+            .then(() => {
+                console.log("Connected")
+                
+                let device = new UM34C(serial)
+                
+                device.on("read", (data) => {
+                    console.log(data)
+                })
+
+                device.readEvery(1000)
+
+                setTimeout(async () => {
+                    await device.terminate()
+                    console.log("Terminated")
+                }, 5000)
+            })
+            .catch(err => console.log("Error:", err))
+
+        return true
+    }
+
+    return false
+}
+
 async function main() {
     const serial = new BluetoothClassicSerialportClient()
 
     const MAX_SCANS = 10
     let nb_scans = 0
-    let opened = false
-    while (nb_scans < MAX_SCANS && !opened) {
+    let connected = false
+    while (nb_scans < MAX_SCANS && !connected) {
         nb_scans++
 
         console.log("Scanning...")
         let devices = await serial.scan()
 
-        // console.log(devices)
         for (let device of devices) {
             console.log(device)
-            if (device .name === "UM34C") {
-
-                serial.connect(device.address)
-                    .then(async () => {
-                        console.log("Connected")
-                        
-                        let device = new UM34C(serial)
-                        
-                        device.on("read", (data) => {
-                            console.log(data)
-                        })
-
-                        device.readEvery(1000)
-
-                        setTimeout(async () => {
-                            await device.terminate()
-                        }, 5000)
-                    })
-                    .catch(err => console.log("Error:", err))
-
-                opened = true
-                break
-            }
+            connected = attemptConnect(serial, device)
+            if (connected) break
         }
     }
 
