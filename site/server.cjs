5c6a651cd435b194a276ccc3c4e8f304008a43ec
SamuelDarras <slamicote@gmail.com>
2022-04-25 12:29:58 +0200

    removed old site

--- site/server.cjs
+++
@@ -1,9 +0,0 @@
-const ws = require("ws")
-
-const wss = new ws.Server({ port: process.env.PORT || 4000 })
-
-console.log(`Server started on port ${wss.address().port}`)
-
-module.exports = {
-    wss
-}
