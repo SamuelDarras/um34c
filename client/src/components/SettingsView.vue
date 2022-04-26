e14fcabced87f1296e29102b3742bdc3fc391708
SamuelDarras <slamicote@gmail.com>
2022-04-26 10:51:33 +0200

    added an empty view for settings

---
+++ client/src/components/SettingsView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card class="ma-6">
+      <v-card-title>
+          Paramètres
+      </v-card-title>
+      <v-card-text>
+          
+      </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+    name: "SettingsView",
+    props: ["data"]
+}
+</script>
+
+<style>
+
+</style>
