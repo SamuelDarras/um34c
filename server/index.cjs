b4a0a883993110953a796ad82dec83e52f22b36d
SamuelDarras <slamicote@gmail.com>
2022-05-04 09:23:06 +0200

    added an export action to the controller to convert JSON to csv

--- server/index.cjs
+++ server/index.cjs
@@ -1,5 +1,6 @@
 const { Controller } = require("./controller.cjs")
 const { Recorder } = require("./recorder.cjs")
+const { Parser, transforms: { flatten } } = require("json2csv")
 const site = require("./server.cjs")
 
 let recorder = new Recorder()
@@ -38,7 +39,7 @@ async function main() {
                 controller.info("connecting")
                 controller.connect(data.addr)
                     .then(device => {
-                        controller.success("connect", {addr: data.addr})
+                        controller.success("connect", { addr: data.addr })
                         device.on("read", data => {
                             controller.send("data", recorder.append(data))
                         })
@@ -47,7 +48,7 @@ async function main() {
                     .catch(err => {
                         console.error(err)
                         controller.error(err, "connect")
-                    })              
+                    })
             })
             .on("disconnect", data => {
                 controller.disconnect()
@@ -78,19 +79,39 @@ async function main() {
             })
             .on("readOn", () => {
                 controller.device.readEvery(controller.device.rate)
+                recorder.new()
             })
             .on("readOff", () => {
                 controller.device.readEvery(0)
-                recorder.new()
+            })
+            .on("export", () => {
+                recorder.export((data) => {
+                    options = {
+                        fields: [
+                            "timestamp.fromStart",
+                            "current",
+                            "voltage"
+                        ],
+                        transforms: [
+                            flatten()
+                        ],
+                    }
+                    let parser = new Parser(options)
+                    console.log(data)
+                    // data = parser.preprocessData(data)
+                    // console.log(data)
+                    let csv = parser.parse(data)
+                    console.log(csv)
+                })
             })
 
-        ws.on('error',function(e){ return console.log(e)})
-        ws.on('close',function(e){ return console.log('websocket closed', e)})
+        ws.on('error', function (e) { return console.log(e) })
+        ws.on('close', function (e) { return console.log('websocket closed', e) })
     })
 
     isReady = true
     site.wss.clients.forEach(client => {
-        client.send(JSON.stringify({type: "ready"}))
+        client.send(JSON.stringify({ type: "ready" }))
     })
 }
 
